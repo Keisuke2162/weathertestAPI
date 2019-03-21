07d6040ed933a4b4476f9ad26023f4b8d83660d8
Keisuke2162 <applabo.kei@gmail.com>
2019-03-21 20:28:41 +0900

    Initial Commit

---
+++ weathertestAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weathertestAPI
+//
+//  Created by 植田圭祐 on 2019/03/21.
+//  Copyright © 2019 Keisuke Ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
